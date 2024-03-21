af3ee34c5254ef9cf3d9e641675659b82b82a082
pelleservan <pelle.servan@hotmail.fr>
2024-03-21 16:45:08 +0100

    start homa page

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -11,7 +11,52 @@ export default function Page() {
     <React.Fragment>
 
         <div id='container'>
-          <h2>Home</h2>
+          <h1>Servan PELLÉ - Data Scientist</h1>
+
+          <p>Wellcome into my portfolio !</p>
+
+          <div>
+            
+            <p>
+              I'm a <span>Data Scientist</span> with a strong passion for <span>Artificial Intelligence</span> and its applications. Having spent three years as an apprentice 
+              between <span>IMT - Télécom Saint-Étienne</span> and <span>STMicroelectronics</span>, I am eager to deepen my knowledge through a <span>PhD</span> focused on 
+              <span>Artificial Intelligence</span>.
+            </p>
+
+            <p>
+              After two years in a <span>CPGE</span> preparatory class, I sought to transition into more practical training. This led me to join <span>IMT - Télécom Saint-Étienne</span> 
+              to pursue a <span>Data Engineer</span> program. <span>Data Engineering</span> involves designing, building, and maintaining infrastructures and systems for collecting, storing, processing, 
+              and analyzing large amounts of data. Throughout this training, I acquired <span>Expertise</span> in setting up and managing data pipelines, databases, data warehouses, and the necessary 
+              tools and technologies to ensure data accessibility, reliability, security, and readiness for analysis by data analysts and scientists. In summary, this program has equipped me 
+              with the skills needed to ensure <span>Data Availability and Quality</span>, empowering analytical processes and decision support within organizations.
+            </p>
+
+            <p>
+              While undergoing this alternating training program, I had the opportunity to join the <span>Research and Development</span> department at <span>STMicroelectronics</span>. 
+              This three-year experience allowed me, firstly, to apply the concepts studied in class in a concrete manner, and also to enhance my knowledge and skills to better meet 
+              the imposed requirements. This experience stands as one of the most significant milestones in my journey for.
+            </p>
+
+          </div>
+
+          <div id='sections'>
+            <div>
+              <h2>Resume</h2>
+              <img src="img/cv.jpg" alt="" />
+            </div>
+
+            <div>
+              <h2>Project</h2>
+              <img src="img/project.jpg" alt="project img" />
+            </div>
+
+            <div>
+              <h2>Contact</h2>
+              <img src="img/contact.jpg" alt="" />
+            </div>
+          </div>
+          
+
         </div>
 
     </React.Fragment>
