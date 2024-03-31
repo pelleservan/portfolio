905dc80b4f44c0a1251830dffeec04a8ad8cd56c
pelleservan <pelle.servan@hotmail.fr>
2024-03-31 13:49:37 +0200

    start project tab

--- portfolio/app/project/page.tsx
+++ portfolio/app/project/page.tsx
@@ -1,13 +1,31 @@
+// import React from "react";
+
+// export default function Page() {
+//     return(
+//         <React.Fragment>
+
+//                 <div id='container'>
+//                     <h2>Projects</h2>
+//                 </div>
+
+//         </React.Fragment>
+//     )
+//   }
+
+
 import React from "react";
 
 export default function Page() {
-    return(
-        <React.Fragment>
-
-                <div id='container'>
-                    <h2>Projects</h2>
-                </div>
-
-        </React.Fragment>
-    )
-  }
+  return (
+    <div id="container">
+      <h2>Projects</h2>
+      <iframe
+        id="notebook"
+        src="https://nbviewer.org/github/pelleservan/cnn_on_reduced_mnist/blob/main/mnist_semi_supervised.ipynb"
+        style={{ width: "100%", height: "800px", border: "none" }} 
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
+}
