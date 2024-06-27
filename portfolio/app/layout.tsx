285764b2aff6243b57f6bb20fa865d5fe1a64477
pelleservan <pelle.servan@hotmail.fr>
2024-06-27 12:14:36 +0200

    add google manager

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -9,17 +9,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Your Website Title</title>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K7VRM4NX');
+          `,
+        }} />
+      </head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7VRM4NX"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <div id='header'>
-            <Header></Header>
+          <Header />
         </div>
-
         <div id="container" style={{ height: "100%" }}>
-          { children }
+          {children}
         </div>
-
         <div id='footer'>
-          <Footer></Footer>
+          <Footer />
         </div>
       </body>
     </html>
